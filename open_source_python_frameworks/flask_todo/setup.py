30fef04d27e3a31de30511096077c64eda0f3f01
johannesgiorgis <johnny.awg@gmail.com>
2018-07-22 16:51:15 -0700

    add initial open source python frameworks directory

---
+++ open_source_python_frameworks/flask_todo/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+requires = [
+	'flask',
+	'flask-sqlalchemy',
+	'psycopg2',
+]
+
+setup(
+	name='flask_todo',
+	version='0.0',
+	description='A To-Do List built with Flask',
+	author='Johannes Giorgis',
+	keywords='web flask',
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=requires
+)
